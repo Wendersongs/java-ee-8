49e377d54a0e2c567790c7deeb69b3181f5f2148
fagury <thiago@fagury.com.br>
2019-02-19 14:24:10 -0300

    Escopo, App, Req, Sess.

--- src/main/java/servlet/CaveraServlet.java
+++ src/main/java/servlet/CaveraServlet.java
@@ -12,28 +12,32 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-@WebServlet ("/cavera/")
+@WebServlet("/cavera")
 public class CaveraServlet extends HttpServlet {
 
     @Inject
-    private ApplicationBean printableObj1;
+    private ApplicationBean appBean;
 
     @Inject
-    private RequestBean printableObj2;
+    private SessionBean sessBean;
 
     @Inject
-    private SessionBean printableObj3;
+    private RequestBean reqBean;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        System.out.println("ApplicationBean: "+printableObj1.getContador() );
-        System.out.println("RequestBean: "+printableObj2.getContador() );
-        System.out.println("SessionBean: "+printableObj3.getContador() );
+        System.out.println("Funfs!!!");
 
-        resp.getWriter().append("ApplicationBean: "+ printableObj1.getContador() + "\n" );
-        resp.getWriter().append("RequestBean: "+ printableObj2.getContador() + "\n" );
-        resp.getWriter().append("SessionBean: "+ printableObj3.getContador() + "\n" );
+        resp.getWriter().append("<h1>OK Caveira</h1>");
+        resp.getWriter().append("<h1>ApplicationBean: " + appBean.imprimeContador() + "</h1>");
+        resp.getWriter().append("<h1>SessionBean: " + sessBean.imprimeContador() + "</h1>");
+        resp.getWriter().append("<h1>RequestBean: " + reqBean.imprimeContador() + "</h1>");
+
+        //Imprime no console
+        System.out.println("ApplicationBean: " + appBean.imprimeContador());
+        System.out.println("SessionBean: " + sessBean.imprimeContador());
+        System.out.println("RequestBean: " + reqBean.imprimeContador());
+
     }
 }
